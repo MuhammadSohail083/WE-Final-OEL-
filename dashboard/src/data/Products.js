7ac98ed8e9b273fdd5fc8f531b0b397d70c3f290
MuhammadSohail083 <59343517+MuhammadSohail083@users.noreply.github.com>
2022-07-01 10:42:47 +0500

    first commmmit

---
+++ dashboard/src/data/Products.js
@@ -0,0 +1,26 @@
+const products = [
+  {
+    _id: "1",
+    name: "CAR1",
+    image: "/images/pic1.jpg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 112,
+    countInStock: 3,
+    rating: 1,
+    numReviews: 2,
+  },
+  {
+    _id: "2",
+    name: "Car2",
+    image: "/images/pic-2.png",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 600,
+    countInStock: 10,
+    rating: 2,
+    numReviews: 2,
+  },
+];
+
+export default products;
---
+++ server/data/Products.js
@@ -0,0 +1,16 @@
+const products = [
+  {
+    _id: "ObjectId(62be83873aa0354259bd532f)",
+    name: "CAR1",
+    image: "/images/pic1.jpg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 112,
+    countInStock: 3,
+    rating: 1,
+    numReviews: 2,
+  },
+ 
+];
+
+export default products;
